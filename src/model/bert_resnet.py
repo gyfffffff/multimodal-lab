7ab69edb8ea7f6ec3de63dd610ec356a9b0f628d
gyf <charon0707@163.com>
2024-01-25 13:18:23 +0800

    单模态处理

--- src/model/bert_resnet.py
+++ src/model/bert_resnet.py
@@ -21,26 +21,30 @@ class BertResnet(nn.Module):
         
         if self.args.use_image and self.args.use_text:
             self.fusion = nn.Linear(128+128, 3)
-
+        else:
+            self.fc = nn.Linear(128, 3)
         self.relu = nn.ReLU()
         
         
     def forward(self, text_ids, attention_masks, imgs):
         if self.args.use_image:
-            img_feature = self.resnet(imgs)  
-            img_feature = self.img_fc(img_feature)  
-            img_feature = self.relu(img_feature)
+            img_feature = self.resnet(imgs)   # [batchsize, 1000]
+            img_feature = self.img_fc(img_feature)    # [batchsize, 128]
+            img_feature = self.relu(img_feature)      # [batchsize, 128]
         if self.args.use_text:
-            text_feature = self.bert(text_ids, attention_mask=attention_masks).last_hidden_state[:, 0, :]  # [batchsize, 768]
-            text_feature = self.text_fc(text_feature)
-            text_feature = self.relu(text_feature)
+            text_feature = self.bert(text_ids, 
+                           attention_mask=attention_masks).last_hidden_state[:, 0, :]  # [batchsize, 768]
+            text_feature = self.text_fc(text_feature)  # [batchsize, 128]
+            text_feature = self.relu(text_feature)     # [batchsize, 128]
         
         if self.args.use_image and self.args.use_text:
             feature = torch.cat([img_feature, text_feature], dim=1)
             output = self.fusion(feature)
         elif self.args.use_image:
+            img_feature = self.fc(img_feature)
             output = img_feature
         elif self.args.use_text:
+            text_feature = self.fc(text_feature)
             output = text_feature
 
         return output
